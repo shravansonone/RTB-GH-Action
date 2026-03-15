d0317e1d9aa287340764098333ce870b73ab0bf4
shravansonone <104133168+shravansonone@users.noreply.github.com>
2026-03-15 21:42:05 +0530

    Create wrongpython.py

---
+++ wrongpython.py
@@ -0,0 +1,27 @@
+# wrong_script.py
+
+import os
+import math
+
+def calculate_average(numbers):
+    total = 0
+    
+    for n in numbers
+        total = total + n
+    
+    avg = total / len(numbers)
+    
+    return avg
+
+
+def read_file():
+    f = open("data.txt", "r")
+    data = f.read()
+    return data
+
+
+nums = [10, 20, 30]
+
+average = calculate_average(nums)
+
+print("Average value is: " + average)
